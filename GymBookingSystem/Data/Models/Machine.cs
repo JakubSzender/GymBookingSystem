a2f02519b453093c6716241420a675267efdb022
Jakub Szender <VoXerzz@gmail.com>
2021-12-29 17:39:43 +0100

    Database model edited

--- GymBookingSystem/Data/Models/Machine.cs
+++ GymBookingSystem/Data/Models/Machine.cs
@@ -12,14 +12,6 @@ namespace GymBookingSystem.Data.Models
         public virtual ICollection<Reservation> Reservations { get; set; }
 
 
-        List<Machine> machines = new()
-        {
-            new Machine { Name = "Machine1" },
-            new Machine { Name = "Machine2" },
-            new Machine { Name = "Machine3" },
-            new Machine { Name = "Machine4" },
-            new Machine { Name = "Machine5" }
-
-        };
+       
     }
 }
