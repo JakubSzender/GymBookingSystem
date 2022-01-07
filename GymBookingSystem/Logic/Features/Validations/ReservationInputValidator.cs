842af2b7a17fe8c2437be9a8f39c4bd8ca563809
Jakub Szender <jakubszender@gmail.com>
2022-01-07 13:42:24 +0100

    Added working fluent validation for ReservationInput page

---
+++ GymBookingSystem/Logic/Features/Validations/ReservationInputValidator.cs
@@ -0,0 +1,31 @@
+﻿
+using FluentValidation;
+using GymBookingSystem.Data.Models;
+using System;
+
+namespace GymBookingSystem.Logic.Features.Validations
+{
+    public class ReservationInputValidator : AbstractValidator<Reservation>
+    {
+		private TimeSpan openHour = new TimeSpan (10,0,0);
+		private TimeSpan closeHour = new TimeSpan (18,0,0);
+
+		public ReservationInputValidator()
+		{
+			RuleFor(x => x.ReservationDate).NotEmpty()
+				.WithMessage("This field can't be empty");
+			RuleFor(x => x.StartHour).NotEmpty()
+					.WithMessage("This field can't be empty");
+			RuleFor(x => x.StartHour).GreaterThanOrEqualTo(openHour).LessThanOrEqualTo(closeHour)
+					.WithMessage("Please pick hour form 10 to 18");
+			RuleFor(x => x.MachineId).NotEmpty()
+					.WithMessage("Please pick machine");
+			
+		}
+
+		
+	
+	}
+
+
+}
