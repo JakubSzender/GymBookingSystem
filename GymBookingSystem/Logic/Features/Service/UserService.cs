857e4f78501c83b1dad449be51de957c1f7bc0ee
Jakub Szender <jakubszender@gmail.com>
2022-01-10 22:08:33 +0100

    Added components for administration panel

--- GymBookingSystem/Logic/Features/Service/UserService.cs
+++ GymBookingSystem/Logic/Features/Service/UserService.cs
@@ -3,8 +3,12 @@ using GymBookingSystem.Data.Models;
 using GymBookingSystem.Logic.Interfaces.Service;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 using System.Security.Claims;
 using System.Security.Principal;
+using System.Threading.Tasks;
 
 namespace GymBookingSystem.Logic.Features.Service
 {
@@ -12,15 +16,54 @@ namespace GymBookingSystem.Logic.Features.Service
 
     {
         private readonly IHttpContextAccessor _httpContext;
+        private readonly ApplicationDbContext _dbContext;
 
-        public UserService(IHttpContextAccessor httpContext)
+
+        public UserService(IHttpContextAccessor httpContext, ApplicationDbContext dbContext)
         {
             _httpContext = httpContext;
+            _dbContext = dbContext;
         }
+
 
         public string GetUserId()
         {
             return _httpContext.HttpContext.User?.FindFirstValue(ClaimTypes.NameIdentifier);
         }
+
+        public List<ApplicationUser> GetUsers()
+        {
+            return _dbContext.ApplicationUser.ToList();
+
+        }
+
+        public ApplicationUser GetUserById(string id)
+        {
+            var user = _dbContext.Users.SingleOrDefault(x => x.Id == id);
+            return (ApplicationUser)user;
+        }
+        public void DeleteUser(string id)
+        {
+            var user = _dbContext.Users.FirstOrDefault(x => x.Id == id);
+            if (user != null)
+            {
+                _dbContext.Users.Remove(user);
+                _dbContext.SaveChanges();
+            }
+        }
+        
+       
+        public void SaveUser(ApplicationUser user)
+        {
+            if (user.Id == "") _dbContext.Users.Add(user);
+            else _dbContext.Users.Update(user);
+            _dbContext.SaveChanges();
+        }
+
+
+
     }
+
+
 }
+
--- GymBookingSystem/Logic/Interfaces/Service/IUserService.cs
+++ GymBookingSystem/Logic/Interfaces/Service/IUserService.cs
@@ -1,7 +1,14 @@
-﻿namespace GymBookingSystem.Logic.Interfaces.Service
+﻿using GymBookingSystem.Data.Models;
+using System.Collections.Generic;
+
+namespace GymBookingSystem.Logic.Interfaces.Service
 {
     public interface IUserService
     {
         string GetUserId();
+        List<ApplicationUser> GetUsers();
+        ApplicationUser GetUserById(string id);
+        void SaveUser(ApplicationUser User);
+        void DeleteUser(string id);
     }
 }
